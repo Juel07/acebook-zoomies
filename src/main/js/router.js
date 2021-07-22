13c849a41d10f96f6c98426c86d68442ca8abe3b
Goncalo Gordo <goncalogordo@gmail.com>
2021-07-22 11:58:35 +0100

    remove unused code

--- src/main/js/router.js
+++ src/main/js/router.js
@@ -7,12 +7,10 @@ import SignUpForm from './users/signup';
 export default function AppRouter() {
   return (
       <Router>
-          {/* <Navigation loggedIn={loggedIn} userId={userId}/> */}
           <Switch>
               <Route path='/' exact component={() => (<div><PostForm /> <PostsBuilder /></div>)}></Route>
               <Route path='/signup' exact component={SignUpForm}></Route>
           </Switch>
-          {/* <Footer loggedIn={loggedIn} userId={userId}/> */}
       </Router>
   );
 }
