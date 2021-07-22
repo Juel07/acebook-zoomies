b27b5eecbaf7daab22a489f0a692895afb5a3638
Goncalo Gordo <goncalogordo@gmail.com>
2021-07-22 13:51:43 +0100

    add link to signup page

--- src/main/js/router.js
+++ src/main/js/router.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PostsBuilder from './posts/postsBuilder';
 import PostForm from './posts/postForm';
 import SignUpForm from './users/signup';
+import Navigation from './navigation';
 
 export default function AppRouter() {
   return (
       <Router>
+          <Navigation />
           <Switch>
               <Route path='/' exact component={() => (<div><PostForm /> <PostsBuilder /></div>)}></Route>
               <Route path='/signup' exact component={SignUpForm}></Route>
